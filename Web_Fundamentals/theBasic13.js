c9d579a5f4b69091a87818448bc03ec7e3131bf0
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2021-05-06 12:57:51 -0700

    solutions for exercises 4 to 7

--- Web_Fundamentals/theBasic13.js
+++ Web_Fundamentals/theBasic13.js
@@ -36,9 +36,74 @@ console.log("Exercise 4");
 // Create and array with all the odd integers between 1 and 255 (inclusive)
 function arrayWithOdds(){
     let oddArr = [];
-    for(let i=1;i<=10;i+2){
+    for(let i=1;i<=255;i+=2){
+        // console.log(i);
         oddArr.push(i);
     }
     console.log(oddArr);
 }
 arrayWithOdds();
+
+console.log("_".repeat(80));
+console.log("Exercise 5");
+// Given an array and a value Y, count and print the number of values greater than Y
+function greaterThanY(arr, num){
+    let count = 0;
+    for (let i=0; i <arr.length; i++){
+        if (arr[i] > num ) {
+            count++;
+        }
+    }
+    console.log(count);
+}
+greaterThanY([1,3,5,7,9], 3);
+
+console.log("_".repeat(80));
+console.log("Exercise 6");
+// Given an array, print the max, min, and average values for that array
+function maxMinAvg(arr){
+    let totalSum = 0;
+    let max = arr[0];
+    let min = arr[0];
+    for (let i=1; i<arr.length;i++){
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+    }
+    for (let i=1; i<arr.length;i++){
+        if (max < arr[i]) {
+            max = arr[i];
+        }
+    }
+    for (let i=0; i<arr.length;i++){
+        totalSum += i; 
+    }
+    average = totalSum / arr.length;
+    console.log("max: " + max);
+    console.log("min: " + min);
+    console.log("average: " + average);
+}
+maxMinAvg([2,4,6,8,1,3,5,7]);
+
+
+console.log("_".repeat(80));
+console.log("Exercise 7");
+// Replace any negative array values with "Dojo";
+function SwapStringForNegativesValues(arr){
+    for(let i=0;i<arr.length;i++){
+        if (arr[i] <= 0){
+            arr[i] = "Dojo";
+        }
+    }
+    console.log(arr)
+}
+SwapStringForNegativesValues([1,-1,2,-2,3,-3]);
+
+console.log("_".repeat(80));
+console.log("Exercise 8");
+
+console.log("_".repeat(80));
+console.log("Exercise 9");
+
+console.log("_".repeat(80));
+console.log("Exercise 10");
