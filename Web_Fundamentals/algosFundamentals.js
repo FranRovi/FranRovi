3578079408aa3670d93d4f771d4280e5da3f49c4
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2021-05-12 21:55:24 -0700

    dataSufficency algos and finished missing algos in algosFundamentals

--- Web_Fundamentals/algosFundamentals.js
+++ Web_Fundamentals/algosFundamentals.js
@@ -108,30 +108,90 @@ console.log("Actual:", generateCoinChange(72));
 // Implement a die that randomly returns an integer between 1 and 6 (inclusive)
 // Roll a pair of these dice, tracking the statistics until doubles are rolled
 // Display the number of rolls, min, max and average
-// function statictisToDoubles () {
-//     let sum = 0;
-//     for (let i = 0 ; i <= num; i++) {
-//         sum +=i;
-//     }
-//     return sum;
-// }
-// console.log("_".repeat(80));
-// console.log(`Executing: statictisToDoubles()`);
-// console.log("Expected:", );
-// console.log("Actual:", statictisToDoubles());
+function statictisToDoubles () {
+    
+    let dice1 = Math.ceil(Math.random()*6);
+    let dice2 = Math.ceil(Math.random()*6);
+    let numRolls = 1;
+    let min = dice1 + dice2; 
+    let max = dice1 + dice2;
+
+    while (dice1 != dice2) {
+        numRolls++;
+        dice1 = Math.ceil(Math.random()*6);
+        dice2 = Math.ceil(Math.random()*6);
+        if (dice1 + dice2 > max) {
+            max = dice1 + dice2;
+        }
+        if (dice1 + dice2 < min) {
+            min = dice1 + dice2;
+        }
+    }
+    let average = (min + max) / 2;
+    console.log("dice 1: " + dice1 +" dice 2: " + dice2)
+    console.log("number of rolls: " + numRolls);
+    console.log("min: " + min);
+    console.log("max: " + max);
+    console.log("average: " + average);
+}
+console.log("_".repeat(80));
+console.log(`Executing: statictisToDoubles()`);
+statictisToDoubles();
+
 
 // Implement a function that given a number, sums that number's digits
 // repeatedly until the sum is only one digit. Return that one digit result
-function sumToOneDigit (num) {
-    // cast the parameter provided into a list of numbers;
-    let oneDigit = 0
-    let numList = num.toString().split("")
-    for (let i=0;i<numList.length;i++){
-        oneDigit += parseInt(numList[i]);
+function sumToOneDigit(num) {
+    let numToCheck = num;
+    console.log(numToCheck);
+    while (numToCheck > 10){
+        let sumNums = 0;
+        let numList = numToCheck.toString().split("")
+        console.log("numList: " + numList);
+        for (let i=0; i<numList.length;i++){
+            sumNums += Number(numList[i]);
+            console.log(sumNums); 
+        }
+        numToCheck = sumNums;
     }
-    return oneDigit;
+    return numToCheck;
 }
 console.log("_".repeat(80));
-console.log(`Executing: sumToOneDigit()`);
-console.log("Expected:", 25);
+console.log(`Executing: sumToOneDigit(1789)`);
+console.log("Expected:", 7);
 console.log("Actual:", sumToOneDigit(1789));
+
+
+// Implement the Fibonacci function, a famous mathematical equation that generates a numerical
+// sequence such that each number is the sum of the previous to two. The fibonacci at index 0 and 1,
+// coincidentally, have vakues of 0 and 1. Your function should accept an argument of which Fibonacci 
+// number.
+function fibonacci(num) {
+    if (num === 0){
+        return [0]
+    } else if (num === 1) {
+        return [0, 1];
+    }else {
+        fibArr = [0, 1];
+        for (let i=2; i< num; i++){
+            fibArr.push(fibArr[i-2] + fibArr[i-1]);
+        }
+    }
+    return fibArr;
+
+}
+console.log("_".repeat(80));
+console.log(`Executing: fibonacci(6)`);
+console.log("Expected:", "[0,1,1,2,3,5]");
+console.log("Actual:", fibonacci(6));
+
+console.log("_".repeat(80));
+console.log(`Executing: fibonacci(3)`);
+console.log("Expected:", "[0,1,1]");
+console.log("Actual:", fibonacci(3));
+
+function sumNums(arr){
+    for (let i=0; i<arr.length;i++){
+
+    }
+}
---
+++ Web_Fundamentals/dataSufficiency.js
@@ -0,0 +1,51 @@
+// Implement a function that accepts two non-negative integers as arguments. Function
+// should return the last digit of the first number(a) raised to an exponent of the 
+// second number(b).
+function lastDigitofAToTheB(num1, num2){
+    let answer = 1;
+    while (num2 > 0) {
+        answer *= num1;
+        console.log(answer)
+        if (answer > 9){
+            answer = answer.toString().split("");
+            answer = answer[answer.length-1];
+        }
+        num2--
+    }
+    return answer
+}
+console.log("_".repeat(80));
+console.log(`Executing: lastDigitofAToTheB(num1, num2)`);
+console.log("Expected:", 1);
+console.log("Actual:", lastDigitofAToTheB(3, 4));
+
+console.log("_".repeat(80));
+console.log(`Executing: lastDigitofAToTheB(num1, num2)`);
+console.log("Expected:", 2);
+console.log("Actual:", lastDigitofAToTheB(12, 5));
+
+// Create a function that, given the number of seconds since 12:00:00, will print the
+// angles (in degrees) of the hour, minute and second hands. As a quick review, there
+// are 360 degrees in a full arc rotation. Treat "straight-up" 12:00:00 as 0 degrees
+// for all hands. 
+function clockHandAngles(seconds){
+    dictDegrees = {};
+    let hourDegrees = Math.floor(seconds/ 3600);
+    dictDegrees["hourDegrees"] = (hourDegrees / 12) * 360;
+    let minutesDegrees = Math.floor((seconds - (hourDegrees * 3600)) / 60);
+    dictDegrees["minutesDegrees"] = (minutesDegrees / 60) * 360; 
+    let secondsDegrees = Math.floor((seconds - (hourDegrees * 3600 + minutesDegrees * 60)));
+    dictDegrees["secondsDegrees"] = (secondsDegrees / 60) * 360;
+
+    return dictDegrees;
+}
+
+console.log("_".repeat(80));
+console.log(`Executing: clockHandAngles(seconds)`);
+console.log("Expected: ", );
+console.log("Actual:", clockHandAngles(150));
+
+console.log("_".repeat(80));
+console.log(`Executing: clockHandAngles(seconds)`);
+console.log("Expected: ", );
+console.log("Actual:", clockHandAngles(25600));
