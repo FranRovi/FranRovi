c542693ce22577f95de4453c8ac0cf849bc7dbb0
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2021-06-06 11:18:12 -0700

    Initial commit: Udemy React folder course and first project created

---
+++ Udemy/React/first_project/src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <h2>Expense Item here!</h2>
+            <div>
+                <p>March 28th 2021</p>
+            </div>
+            <div>
+                <p>Car Insurance</p>
+            </div>
+            <div>
+                <p>$294.67</p>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
