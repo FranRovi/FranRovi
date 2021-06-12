9bd3eaa98947e985feba658f270a1b5e12b83137
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2021-06-11 18:04:24 -0700

    rBinarySearch algo WIP

--- Algorithms/recursion.js
+++ Algorithms/recursion.js
@@ -31,3 +31,41 @@ console.log("_".repeat(80));
 console.log(`Executing: rSigma(str)`);
 console.log("Expected: 0");
 console.log("Actual:", rSigma(-1));
+
+// Write a recursive function that, given a sorted array and a value, 
+// determines whether the value is found within the array. For example, 
+// rBinarySearch([1,3,5,6],4) = false;  rBinarySearch([1,3,5,6],4) = true.
+
+function rBinarySearch(arr, target){
+    let leftIdx = 0;
+    let rigthIdx = arr.length-1 
+
+
+    while (leftIdx <= rightIdx) {
+        let midpointIdx = leftIdx + Math.floor((rigthIdx - leftIdx) / 2 );
+        let midpointVal = arr[midpointIdx];
+        if (midpointVal === target) {
+            return true;
+        } else {
+
+        }
+    }
+    
+    while (num > 0) {
+        totalSum += num;
+        // console.log(totalSum)
+        num--
+        // console.log(num);
+        rBinarySearch(num);
+    }
+    return totalSum;
+}
+console.log("_".repeat(80));
+console.log(`Executing: rBinarySearch(str)`);
+console.log("Expected: false");
+console.log("Actual:", rBinarySearch([1,3,5,6],4));
+
+console.log("_".repeat(80));
+console.log(`Executing: rBinarySearch(str)`);
+console.log("Expected: true");
+console.log("Actual:", rBinarySearch([1,3,5,6],5));
