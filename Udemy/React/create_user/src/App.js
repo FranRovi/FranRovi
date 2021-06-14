3437127793be1215fd7d4e0924732822e7c0be8e
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2021-06-14 16:38:17 -0700

    Create_user_project: initial commit

---
+++ Udemy/React/create_user/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import AddUser from './components/Users/AddUser';
+
+function App() {
+  return (
+    <div>
+      {/* <h1>Hello World!</h1> */}
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
