99182f5b4068f290cdbf9ad8006141e53ad212e5
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2021-07-01 22:44:57 -0700

    http_request project: connecting app to Firebase. Get and Post requests examples

--- Udemy/React/http_request/src/App.js
+++ Udemy/React/http_request/src/App.js
@@ -1,29 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
+import AddMovie from './components/AddMovie';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   // CODE BELOW IS AN ALTERNATIVE WAY OR WRITING .THEN CHAIN CALLS (PROMISES)
   // NONETHELESS IS STILL ASYNC FUNCTIONS, THEY LOOK LIKE SYNCHRONOUS
 
-  async function fetchMoviesHandler() {
-    const response = await fetch('https://swapi.dev/api/films/');
-    const data = await response.json();
-
-    const transformedMovies = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        releaseDate: movieData.release_date,
-        openingText: movieData.opening_crawl
+  
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      // STAR WARS API
+      // const response = await fetch('https://swapi.dev/api/films/');
+      
+      // FIREBASE DATABASE THROUGH API
+      const response = await fetch('https://react-http-e45a9-default-rtdb.firebaseio.com/movies.json');
+      
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+        
       }
-    })
-    setMovies(transformedMovies);
-  };   
+      const data = await response.json();
 
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          releaseDate: data[key].releaseDate,
+          openingText: data[key].openingText,
+        });
+      }
+      
+      // const transformedMovies = data.results.map(movieData => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     releaseDate: movieData.release_date,
+      //     openingText: movieData.opening_crawl
+      //   }
+      // })
+      // setMovies(transformedMovies);
+
+      setMovies(loadedMovies);
+    } catch (error){
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);   
+  
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+  
   // CHAIN CALL .THEN CODE EXAMPLE
   
   // const fetchMoviesHandler = () => {
@@ -60,13 +97,47 @@ function App() {
   //   },
   // ];
 
+  async function addMovieHandler(movie) {
+    console.log(movie);
+    const response = await fetch('https://react-http-e45a9-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+  
+  let content = <p>Found no movies.</p>
+  
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+  
+  if (error) {
+    content = <p>{error}</p>
+  }
+
+  if (isLoading) {
+    content = <p>Loading ... </p>
+  }
+
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {/* {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && <p>No movies to display yet, please click the button above.</p>}
+        {isLoading && <p>Loading ... </p>}
+        {!isLoading && error && <p>{error}</p>} */}
+        {content}
       </section>
     </React.Fragment>
   );
