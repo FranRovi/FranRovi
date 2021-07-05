263e9feb6652904f97940262bd923083bf4d167e
Francisco Rovirosa <rovirosafrancisco@gmail.com>
2021-07-04 23:01:35 -0700

    portfolio project: Styling MainHeader component

--- Udemy/React/portfolio/src/components/MainHeader/MainHeader.js
+++ Udemy/React/portfolio/src/components/MainHeader/MainHeader.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import classes from './MainHeader.module.css';
+import profilePic from '../../assets/profile.jpg';
 
 const MainHeader = (props) => {
+    const [aboutMeIsClicked, setAboutMeIsClicked] = useState(false);
+
+    const aboutMeClickHandler = () => {
+        setAboutMeIsClicked((prevAboutMe) => !prevAboutMe);
+    };
+    console.log(aboutMeIsClicked);
+
     return(
         <header className={classes['main-header']}>
             {/* <nav className={`${classes['main-row']} ${classes['main-links-center']}`}> */}
             <nav className={`${classes['main-row']} ${classes['main-links-center']}`}>
+                <div>
+                    <img src={profilePic} alt="portrait of myself" className={classes['main-image']}/>
+                </div>
                 <div>   
                     <h1 className={classes['main-mr']}>Francisco Rovirosa</h1>
                     <h4>Full-Stack Developer</h4>
@@ -15,7 +26,7 @@ const MainHeader = (props) => {
                     <div className={classes['main-links']}>
                         {/* <li className={classes['main-link-white']}></li>
                         <li className={classes['main-link-white']}></li> */}
-                        <li className={classes['main-link-white']}>About Me</li>
+                        {<button className={classes['main-link-white']} aboutMeIsClicked={aboutMeIsClicked} onClick={aboutMeClickHandler}>About Me</button>}
                         <li className={classes['main-link-white']}>Projects</li>
                         <li className={classes['main-link-white']}>Contact</li>
                     </div>
